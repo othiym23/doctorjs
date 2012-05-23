b9dd680185e3034a0f7b2ba6cb262c0260e51fc0
Forrest L Norvell <forrest@newrelic.com>
2012-05-23 15:57:07 -0700

    Whitespace cleanup.

--- bin/jsctags.js
+++ bin/jsctags.js
@@ -48,54 +48,58 @@ var path   = require('path')
 var argv = process.argv;
 
 function usage() {
-    util.puts("usage: jsctags [options] path0 [.. pathN]");
-    util.puts("options:");
-    util.puts("    -f, --output file     place output in the given file (-f " +
-             "- for stdout)");
-    util.puts("    -h, --help            display this usage info");
-    util.puts("    -j, --jsonp function  use JSONP with a function name");
-    util.puts("    -o, --output file     synonym for -f");
-    util.puts("        --oneprog         combine all inputs into one program");
-    util.puts("    -L, --libroot dir     add a CommonJS module root (like " +
-             "require.paths)")
-    util.puts("    -W, --warning level   set log level (debug/info/warn/" +
-             "error, default error)");
-    process.exit(1);
+  util.puts("usage: jsctags [options] path0 [.. pathN]");
+  util.puts("options:");
+  util.puts("    -f, --output file     place output in the given file (-f " +
+            "- for stdout)");
+  util.puts("    -h, --help            display this usage info");
+  util.puts("    -j, --jsonp function  use JSONP with a function name");
+  util.puts("    -o, --output file     synonym for -f");
+  util.puts("        --oneprog         combine all inputs into one program");
+  util.puts("    -L, --libroot dir     add a CommonJS module root (like " +
+            "require.paths)");
+  util.puts("    -W, --warning level   set log level (debug/info/warn/" +
+            "error, default error)");
+  process.exit(1);
 }
 
 var opts;
 try {
-    opts = getopt("help|h", "jsonp|j=s", "libroot|L=s@", "oneprog", "output|o|f=s",
-                  "sort|=s", "warning|W=s");
-} catch (e) {
-    util.puts(e);
-    usage();
+  opts = getopt("help|h",
+                "jsonp|j=s",
+                "libroot|L=s@",
+                "oneprog",
+                "output|o|f=s",
+                "sort|=s",
+                "warning|W=s");
+}
+catch (e) {
+  util.puts(e);
+  usage();
 }
 
 var pathCount = argv.length - 2;
-if (opts.help || pathCount === 0) {
-    usage();
-}
+if (opts.help || pathCount === 0) usage();
 
 if (opts.warning) {
-    var level = opts.warning.toUpperCase();
-    if (!log.levels.hasOwnProperty(level)) {
-        util.puts("no such logging level: \"" + opts.warning + "\"");
-        usage();
-    }
-    log.level = log.levels[level];
+  var level = opts.warning.toUpperCase();
+  if (!log.levels.hasOwnProperty(level)) {
+    util.puts("no such logging level: \"" + opts.warning + "\"");
+    usage();
+  }
+  log.level = log.levels[level];
 }
 
 function idFor(st) {
-    return st.dev + "," + st.ino;
+  return st.dev + "," + st.ino;
 }
 
 var librootIds = {};
 if ('libroot' in opts) {
-    opts.libroot.forEach(function(libroot) {
-        var st = fs.statSync(libroot);
-        librootIds[idFor(st)] = true;
-    });
+  opts.libroot.forEach(function(libroot) {
+    var st = fs.statSync(libroot);
+    librootIds[idFor(st)] = true;
+  });
 }
 
 var tags = new Tags();
@@ -103,109 +107,102 @@ var tags = new Tags();
 // Ascend the directory hierarchy to find the CommonJS package this module is
 // in, if any.
 function getModuleInfo(fullPath) {
-    function commonJS(modulePath, packageId) {
-        var ext = path.extname(modulePath), len = modulePath.length;
-        var modulePart = modulePath.substring(0, len - ext.length);
-        var moduleId = (packageId != null)
-                       ? packageId + ":" + modulePart
-                       : modulePart;
-        return { commonJS: true, module: moduleId };
+  function commonJS(modulePath, packageId) {
+    var ext = path.extname(modulePath), len = modulePath.length;
+    var modulePart = modulePath.substring(0, len - ext.length);
+    var moduleId = (packageId !== null) ? packageId + ":" + modulePart : modulePart;
+    return { commonJS: true, module: moduleId };
+  }
+
+  var dir = path.dirname(fullPath);
+  var i = 0, lastPath = null, libPath = null;
+  while (true) {
+    var p, st;
+    try {
+      p = dir;
+      _(i).times(function() { p = path.dirname(p); });
+      st = fs.statSync(p);
+    }
+    catch (e) {
+      break;
     }
 
-    var dir = path.dirname(fullPath);
-    var i = 0, lastPath = null, libPath = null;
-    while (true) {
-        var p, st;
-        try {
-            p = dir;
-            _(i).times(function() { p = path.dirname(p); });
-            st = fs.statSync(p);
-        } catch (e) {
-            break;
-        }
+    if (p === lastPath) {
+      break;
+    }
+    lastPath = p;
 
-        if (p === lastPath) {
-            break;
-        }
-        lastPath = p;
+    var metadataPath = path.join(p, "package.json");
+    try {
+      var metadata = fs.readFileSync(metadataPath);
+      var packageId = JSON.parse(metadata).name;
+      if (typeof(packageId) !== 'string') {
+        // Fall back to the directory name in case we're in a package
+        // that doesn't conform to the CommonJS spec, such as a Bespin
+        // plugin.
+        packageId = path.basename(p);
+      }
 
-        var metadataPath = path.join(p, "package.json");
-        try {
-            var metadata = fs.readFileSync(metadataPath);
-            var packageId = JSON.parse(metadata).name;
-            if (typeof(packageId) !== 'string') {
-                // Fall back to the directory name in case we're in a package
-                // that doesn't conform to the CommonJS spec, such as a Bespin
-                // plugin.
-                packageId = path.basename(p);
-            }
+      if (libPath === null) {
+        // We're in a nonconformant (Bespin-style) package lacking a
+        // "lib" directory. Assume that the module files are rooted
+        // alongside the package.json file.
+        libPath = p;
+      }
 
-            if (libPath == null) {
-                // We're in a nonconformant (Bespin-style) package lacking a
-                // "lib" directory. Assume that the module files are rooted
-                // alongside the package.json file.
-                libPath = p;
-            }
+      return commonJS(fullPath.substring(libPath.length + 1), packageId);
+    }
+    catch (e) {}
 
-            return commonJS(fullPath.substring(libPath.length + 1), packageId);
-        } catch (e) {}
-
-        if (path.basename(p) === "lib") {
-            libPath = p;
-        }
-
-        if (librootIds.hasOwnProperty(idFor(st))) {
-            return commonJS(fullPath.substring(p.length + 1));
-        }
-
-        if (p.lastIndexOf("/") < 1) {
-            // For some reason path.dirname() returns "." once it's done.
-            break;
-        }
-
-        i++;
+    if (path.basename(p) === "lib") {
+      libPath = p;
     }
 
-    return { commonJS: false };
+    if (librootIds.hasOwnProperty(idFor(st))) {
+      return commonJS(fullPath.substring(p.length + 1));
+    }
+
+    if (p.lastIndexOf("/") < 1) {
+      // For some reason path.dirname() returns "." once it's done.
+      break;
+    }
+
+    i++;
+  }
+
+  return {commonJS: false};
 }
 
 var idsSeen = {};
 function processPath(p) {
-    var st = fs.statSync(p);
-    var id = idFor(st);
-    if (id in idsSeen) {
-        return; // avoid loops
-    }
-    idsSeen[id] = true;
+  var st = fs.statSync(p);
+  var id = idFor(st);
+  if (id in idsSeen) return; // avoid loops
 
-    var ext = path.extname(p).toLowerCase();
-    if (st.isDirectory()) {
-        fs.readdirSync(p).forEach(function(filename) {
-            processPath(path.join(p, filename));
-        });
-    } else if (ext === ".js" || ext === ".jsm") {
-        try {
-            var data = fs.readFileSync(p, "utf8");
-            tags.scan(data, p, getModuleInfo(p));
-        } catch (e) {
-            if ('lineNumber' in e) {
-                util.puts("error:" + p + ":" + e.lineNumber + ": " + e);
-            } else {
-                util.puts(e.message);
-                throw e;
-            }
-        }
-    }
-}
+  idsSeen[id] = true;
 
-if (opts.oneprog)
-  processMany();
-else
-  for (var i = 0; i < pathCount; i++) {
-    //processPath(argv[i + 2], false, "", "");
-    //dimvar: processPath seems to use its first arg only
-    processPath(argv[i + 2]);
+  var ext = path.extname(p).toLowerCase();
+  if (st.isDirectory()) {
+    fs.readdirSync(p).forEach(function(filename) {
+      processPath(path.join(p, filename));
+    });
   }
+  else if (ext === ".js" || ext === ".jsm") {
+    try {
+      var data = fs.readFileSync(p, "utf8");
+      tags.scan(data, p, getModuleInfo(p));
+    }
+    catch (e) {
+      if ('lineNumber' in e) {
+        util.puts("error:" + p + ":" + e.lineNumber + ": " + e);
+      }
+      else {
+        util.puts(e.message);
+        throw e;
+      }
+    }
+  }
+}
 
 function processMany() {
   var i, fileinfo = new Array(pathCount), f, linecount = 0, l, data,
@@ -214,14 +211,16 @@ function processMany() {
   for (i = 0; i <= pcm1; i++) {
     f = argv[i + 2];
     data = readf(argv[i + 2], "utf8");
-    if (data.charAt(data.length - 1) === "\n")
+
+    if (data.charAt(data.length - 1) === "\n") {
       l = data.split("\n").length - 1;
+    }
     else {
       l = data.split("\n").length;
       data += "\n";
     }
-    fileinfo[i] = {path: f, lines: l, data: data,
-                   start: linecount + 1, end: linecount + l};
+
+    fileinfo[i] = {path: f, lines: l, data: data, start: linecount + 1, end: linecount + l};
     linecount += l;
   }
 
@@ -234,12 +233,15 @@ function processMany() {
     while (true) {
       i = floor((s + e) / 2);
       f = fileinfo[i];
-      if (ln < f.start)
+      if (ln < f.start) {
         e = i;
-      else if (ln >= f.start && ln <= f.end)
+      }
+      else if (ln >= f.start && ln <= f.end) {
         return {tagfile: f.path, lineno: ln - f.start + 1};
-      else
+      }
+      else {
         s = i + 1;
+      }
     }
   }
 
@@ -251,25 +253,40 @@ function processMany() {
   }
 }
 
+if (opts.oneprog) {
+  processMany();
+}
+else {
+  for (var i = 0; i < pathCount; i++) {
+    //processPath(argv[i + 2], false, "", "");
+    //dimvar: processPath seems to use its first arg only
+    processPath(argv[i + 2]);
+  }
+}
+
 var out;
 if (opts.hasOwnProperty('output')) {
-    if (opts.output === '-') {
-        out = process.stdout;
-    } else {
-        out = fs.createWriteStream(opts.output);
-    }
-} else if (opts.hasOwnProperty('jsonp')) {
-    out = fs.createWriteStream("tags.jsonp");
-} else {
-    out = fs.createWriteStream("tags");
+  if (opts.output === '-') {
+    out = process.stdout;
+  }
+  else {
+    out = fs.createWriteStream(opts.output);
+  }
+}
+else if (opts.hasOwnProperty('jsonp')) {
+  out = fs.createWriteStream("tags.jsonp");
+}
+else {
+  out = fs.createWriteStream("tags");
 }
 
 if (opts.hasOwnProperty('jsonp')) {
-    tags.writeJSONP(out, opts.jsonp);
-} else {
-    tags.write(out, opts);
+  tags.writeJSONP(out, opts.jsonp);
+}
+else {
+  tags.write(out, opts);
 }
 
-if (out != process.stdout) {
-    out.end();
+if (out !== process.stdout) {
+  out.end();
 }
